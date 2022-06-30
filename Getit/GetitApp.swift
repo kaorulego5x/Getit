8ba1f90a270debd75c606f9fa253da5537cb5e24
kaorulego5x <kaorulego5x@gmail.com>
2022-06-30 15:58:19 +0900

    Initial Commit

---
+++ Getit/GetitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetitApp.swift
+//  Getit
+//
+//  Created by kaorulego5x on 2022/06/30.
+//
+
+import SwiftUI
+
+@main
+struct GetitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
